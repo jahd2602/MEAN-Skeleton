a3f154bc9e244bf49a928ffd38afad99fcf90620
Andrew D'Amelio <dev@andrewdamel.io>
2014-05-31 17:56:09 -0400

    updated

--- public/js/app.js
+++ public/js/app.js
@@ -6,10 +6,10 @@ app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $lo
 	$routeProvider
 		.when('/', {templateUrl: '/partials/main.html', controller:  'MainCtrl'	})
 		.when('/view', {templateUrl: '/partials/view.html', controller:  'MainCtrl'	})
+	    .otherwise({redirectTo: '/'});
 
 }]);
 
 app.controller('MainCtrl', ['$scope', function ($scope) {
 	$scope.myVar = 'angular is working!';
 }]);
-
