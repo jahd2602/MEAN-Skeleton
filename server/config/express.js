3547f9299dc02cdb119cb51f2f1804a369e913f1
andrewdamelio <dev@andrewdamel.io>
2014-08-22 03:04:28 -0400

    minor updates

--- server/config/express.js
+++ server/config/express.js
@@ -6,8 +6,15 @@ var express = require('express'),
 module.exports = function(app, config) {
 	app.set('views', config.rootPath + '/server/views');
 	app.set('view engine', 'jade');
-	app.use(bodyParser())
-	app.use(morgan())
-	morgan({ format: 'dev', immediate: true })
+
+	app.use(bodyParser.json({
+  		extended: true
+	}));
+
+	app.use(morgan('combined'))
+	// only log error responses
+	morgan('combined', {
+	  skip: function (req, res) { return res.statusCode < 400 }
+	})
 	app.use(express.static(config.rootPath + '/public'));
 };
